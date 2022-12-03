945ca144a112da060f67f6d5f6915cd101422be2
Diễn <phixuandien@gmail.com>
2022-12-03 11:09:52 +0700

    create project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./styles/scss/styles.scss";
 
-function App() {
+import DefaultLayout from "layout/DefaultLayout";
+import LoginPage from "views/Login";
+import { RouteBase } from "constants/routeUrl";
+
+const App: React.FC = () => {
+  //! Render
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path={RouteBase.Login} exact component={LoginPage} />
+        <Route path={RouteBase.Home} component={DefaultLayout} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
